9ff24c0eccc877022a63cacaa60da381a94d5068
Renan Moura Batista <201219020214@DOMINIO.FACOM.UFMS.BR>
2016-12-15 19:46:22 -0300

    criado o arquivo index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+
+var math = {
+	soma : function (){},
+    subtracao : function(){},
+    multiplicacao : function (){},
+    divisao: function (){}, 
+    fatorial: function (){}, 
+    combinacao: function() {}, 
+    arranjo: function (){}, 
+    permutacao: function(){}, 
+    numeroPrimo: function(){}, 
+    converteBinario: function(){}
+}
