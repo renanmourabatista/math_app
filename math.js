af05fb60953fbb35228a14c5404ff20598d92d62
Renan Moura Batista <201219020214@DOMINIO.FACOM.UFMS.BR>
2016-12-15 19:58:55 -0300

    adicionada a funcao do numero primo

--- math.js
+++ math.js
@@ -8,6 +8,19 @@ var math = {
     combinacao: function() {}, 
     arranjo: function (){}, 
     permutacao: function(){}, 
-    numeroPrimo: function(){}, 
+    numeroPrimo: function(numero){
+        var metade =  parseInt(numero / 2);
+        
+        while(metade > 1) {
+            if(numero % metade == 0){
+                return false;
+            }
+
+            metade--;
+        }
+
+        return true;
+
+    }, 
     converteBinario: function(){}
 }
