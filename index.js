90e8008f83a10becf5c2462f833934127e893e4c
Rallristhy Alves <201219020575@DOMINIO.FACOM.UFMS.BR>
2016-12-15 20:29:55 -0300

    fatorial

--- index.js
+++ index.js
@@ -4,10 +4,17 @@ var math = {
     subtracao : function(){},
     multiplicacao : function (){},
     divisao: function (){}, 
-    fatorial: function (){}, 
+    fatorial: function (x){
+        var fat = 1;
+        while(x >= 1){
+          fat = fat * x;
+          x--;
+        }
+        return fat;
+    }, 
     combinacao: function() {}, 
     arranjo: function (){}, 
     permutacao: function(){}, 
-    numeroPrimo: function(){}, 
+    numeroPrimo: function(){},
     converteBinario: function(){}
 }
